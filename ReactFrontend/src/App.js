aa8e0b28c4301458d8f8bb2c025c4c7b80203ec2
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-07-09 12:09:09 +0530

    #package.json

--- ReactFrontend/src/App.js
+++ ReactFrontend/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@chakra-ui/react';
 function App() {
   return (
     <div className="App">
-     
+    <Button colorScheme='blue'>Button</Button>
     </div>
   );
 }
