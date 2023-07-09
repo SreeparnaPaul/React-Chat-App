13c4a6264e361d46535ccaa1933653b36c5a9216
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-07-09 12:33:16 +0530

    basic api integration

--- ReactFrontend/src/App.js
+++ ReactFrontend/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from '@chakra-ui/react';
+import { Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import ChatPage from './Pages/ChatPage';
 function App() {
   return (
     <div className="App">
-    <Button colorScheme='blue'>Button</Button>
+      <Route exact path='/' component={HomePage}/>
+      <Route path = "/chats" component={ChatPage}/>
     </div>
   );
 }
