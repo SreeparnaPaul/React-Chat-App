8cfbc94e0a98fe1ff31af911b9ca96d1a2502c27
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-07-13 21:18:58 +0530

    #HomePage

--- ReactFrontend/src/Pages/HomePage.js
+++ ReactFrontend/src/Pages/HomePage.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import {Box, Container,Text} from '@chakra-ui/react'
+import {Box, Container,Text,Tabs,TabPanels,TabList,TabPanel,Tab} from '@chakra-ui/react'
 
 function HomePage() {
   return (
     <Container maxW='xl' centerContent>
       <Box d='flex' justifyContent='center' p={3} bg={"white"} w="100%" m = "40px 0 15px 0" borderRadius="15px" borderWidth='1px'>
-        <Text d='flex' justifyContent='center' fontSize='4xl' fontFamily='Work sans' color='black'>Chit-Chat</Text>
+        <Text fontSize='4xl' fontFamily='Work sans' color='black' style={{display:"flex",justifyContent:"center"}}>Chit-Chat</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius='lg' color='black' borderWidth='1px'>
+        <Tabs variant='soft-rounded' >
+          <TabList>
+            <Tab width={'50%'}>Login</Tab>
+            <Tab width={'50%'}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+          <TabPanel>
+          
+          </TabPanel>
+          <TabPanel>
+          
+          </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Box>
     </Container>
   )
