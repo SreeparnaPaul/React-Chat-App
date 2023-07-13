4f234a298006b3e7a79a65075132eea5576cdf93
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-07-13 21:04:16 +0530

    #chatBackground,app.css

--- ReactFrontend/src/Pages/HomePage.js
+++ ReactFrontend/src/Pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {Box, Container,Text} from '@chakra-ui/react'
 
 function HomePage() {
   return (
-    <div>
-      Home
-    </div>
+    <Container maxW='xl' centerContent>
+      <Box d='flex' justifyContent='center' p={3} bg={"white"} w="100%" m = "40px 0 15px 0" borderRadius="15px" borderWidth='1px'>
+        <Text d='flex' justifyContent='center' fontSize='4xl' fontFamily='Work sans' color='black'>Chit-Chat</Text>
+      </Box>
+    </Container>
   )
 }
 
