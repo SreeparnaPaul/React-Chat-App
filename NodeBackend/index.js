91a887a1b8613cc451d5c74f74ea44738a8d3e60
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-07-13 22:59:40 +0530

    #db connection

--- NodeBackend/index.js
+++ NodeBackend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const {chats} =require("./data/data")
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const app = express();
 dotenv.config()
+connectDB()
 const PORT = process.env.PORT || 5000
 app.get('/',(req,res)=>{
     res.send("API is running")
