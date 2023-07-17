aaf691e5d0ea4e6dfce136b35ca01f58021d327b
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-07-17 20:12:06 +0530

    #errorMiddleware

--- NodeBackend/index.js
+++ NodeBackend/index.js
@@ -1,25 +1,21 @@
 const express = require("express");
-const {chats} =require("./data/data")
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes")
+const {notFound,errorHandler} = require("./middlewares/errorMiddleware")
 const app = express();
 dotenv.config()
 connectDB()
 const PORT = process.env.PORT || 5000
 app.use(express.json()); // to accept json data
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+
 
 app.use("/chatApp/user", userRoutes);
-// app.get("/chatApp/chats",(req,res)=>{
-//     res.send(chats)
-// })
 
-// app.get("/chatApp/chats/:id",(req,res)=>{
-//    const singleChat = chats.find((c)=>c._id === req.params.id)
-//    res.send(singleChat)
-// })
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(PORT,console.log(`Server started on Port ${PORT}`));
